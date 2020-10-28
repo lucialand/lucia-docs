a89fc2586558b818c169f5d179a2a5c7baec9b65
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-28 12:31:22 -0700

    fix: changed edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,13 +79,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/lucialand/lucia-docs/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/lucialand/lucia-docs/edit/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
