e33f3c0d9161af16376d97109bb90cb4b64a7334
Aiden Bai <mail.contraband@gmail.com>
2020-11-16 11:33:46 -0800

    fix: remove outline class on main page button

--- src/pages/index.js
+++ src/pages/index.js
@@ -70,7 +70,7 @@ function Home() {
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                'button button--outline button--secondary button--lg',
+                'button button--secondary button--lg',
                 styles.getStarted
               )}
               to={useBaseUrl('docs/essentials/installation')}
