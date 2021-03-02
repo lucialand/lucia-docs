bd80bff8cf3f6b7da3dae7f8aad31f2a99d709c7
Aiden Bai <mail.contraband@gmail.com>
2021-03-01 17:32:53 -0800

    Fix sidebar directives

--- sidebars.js
+++ sidebars.js
@@ -15,7 +15,6 @@ module.exports = {
       'directives/bind-directive',
       'directives/for-directive',
       'directives/model-directive',
-      'directives/init-directive',
       'directives/mask-directive',
     ],
     'Special Properties': [
